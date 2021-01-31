39cc31f48e7cedbbc20639581bd80b65a5d0f616
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-31 16:05:20 -0600

    Create Counter.js component

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+function Counter(props) {
+  return <div></div>
+}
+
+
+const mapStateToProps = (state) => {
+  
+}
+
+const mapDispatchToProps = (state) => {
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Connect);
